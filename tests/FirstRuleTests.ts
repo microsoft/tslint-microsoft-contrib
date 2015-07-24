b7737c6b5fecbef846f40f6d460915e48db8b1c6
HamletDRC <hamletdrc@gmail.com>
2015-07-24 18:20:34 +0200

    added chai assertions

--- tests/FirstRuleTests.ts
+++ tests/FirstRuleTests.ts
@@ -1,8 +1,12 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
+
+import chai = require('chai')
 
 describe('FirstRuleTests', () : void => {
 
     it('should run a test', () : void => {
+        chai.expect(1).to.equal(1);
     });
 
 });

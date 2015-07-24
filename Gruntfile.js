acd1a54d1dca6e67b4c33561dc53cbe93641ea98
HamletDRC <hamletdrc@gmail.com>
2015-07-24 15:58:53 +0200

    Added tslint compilation to build

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            src: ['./dist'],
+            options: {
+                force: true
+            },
+        },
+
+        ts: {
+            default: {
+                src: ['./src/**/*.ts'],
+                outDir: './dist'
+            },
+        }
+    });
+
+    require('load-grunt-tasks')(grunt); // loads all grunt-* npm tasks
+
+    grunt.registerTask('all', 'Performs a cleanup and a full build with all tasks', [
+        'clean',
+        'ts'
+    ]);
+
+};

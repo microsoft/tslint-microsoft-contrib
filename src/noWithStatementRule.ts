9d043b1f6378706d961cd259fb7d436f302ea15a
HamletDRC <hamletdrc@gmail.com>
2015-07-28 16:16:36 +0200

    noWithStatement rule

---
+++ src/noWithStatementRule.ts
@@ -0,0 +1,22 @@
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "Forbidden with statement";
+
+    public apply(sourceFile : ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoWithStatementWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoWithStatementWalker extends Lint.RuleWalker {
+
+    public constructor(sourceFile : ts.SourceFile, options : Lint.IOptions) {
+        super(sourceFile, options);
+    }
+
+    protected visitNode(node: ts.Node): void {
+        if (node.kind === ts.SyntaxKind.WithStatement) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+        }
+        super.visitNode(node);
+    }
+}

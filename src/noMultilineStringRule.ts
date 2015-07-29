a95add3f3250434343932e21a34cf900c6339d70
HamletDRC <hamletdrc@gmail.com>
2015-07-29 07:41:31 +0200

    no multiline string rule

---
+++ src/noMultilineStringRule.ts
@@ -0,0 +1,21 @@
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "Forbidden Multiline string: ";
+
+    public apply(sourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoMultilineStringWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoMultilineStringWalker extends Lint.RuleWalker {
+
+
+    protected visitNode(node: ts.Node): void {
+        if (node.kind === ts.SyntaxKind.NoSubstitutionTemplateLiteral) {
+            let fullText : string = node.getFullText();
+            let firstLine : string = fullText.substring(0, fullText.indexOf('\n'));
+            let trimmed : string = firstLine.substring(0, 40);
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING + trimmed + '...'));
+        }
+        super.visitNode(node);
+    }
+}

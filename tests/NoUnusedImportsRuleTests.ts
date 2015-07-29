0250c5c5869d0b79f31237f705eda59c901d6cf2
HamletDRC <hamletdrc@gmail.com>
2015-07-30 01:10:19 +0200

    added unused import test

--- tests/NoUnusedImportsRuleTests.ts
+++ tests/NoUnusedImportsRuleTests.ts
@@ -1,12 +1,22 @@
 /// <reference path="../typings/mocha.d.ts" />
 /// <reference path="../typings/chai.d.ts" />
 
-import chai = require('chai');
+import TestHelper = require('./TestHelper');
 
 describe('noUnusedImportsRule', () : void => {
 
-    it('should run a test', () : void => {
-        chai.expect(1).to.equal(1);
+    it('should detect an unused import', () : void => {
+        var ruleName : string = 'no-unused-imports';
+        var inputFile : string = 'test-data/NoUnusedImportsTestInput.ts';
+        TestHelper.assertViolations(
+            ruleName,
+            inputFile,
+            [ { "failure": "unused import: 'NoUnusedImportsRule'",
+                "name": "test-data/NoUnusedImportsTestInput.ts",
+                "ruleName": "no-unused-imports",
+                "startPosition": { "character": 7, "line": 2, "position": 40 },
+                "endPosition": { "character": 26, "line": 2, "position": 59 } } ]
+        );
     });
 
 });
---
+++ tests/TestHelper.ts
@@ -0,0 +1,53 @@
+/// <reference path="../typings/node.d.ts" />
+/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+
+import fs = require("fs");
+import chai = require('chai');
+
+module TestHelper {
+
+    export interface FailurePosition {
+        character: number,
+        line: number,
+        position: number
+    }
+    export interface ExpectedFailure {
+        ruleName: string,
+        name: string,
+        failure: string,
+        endPosition: FailurePosition,
+        startPosition: FailurePosition
+    }
+
+    export function assertViolations(ruleName : string, inputFile : string, expectedFailures : ExpectedFailure[]) {
+
+        var configuration = {
+            rules: {}
+        };
+        configuration.rules[ruleName] = true;
+
+        var options : Lint.ILinterOptions = {
+            formatter: 'json',
+            configuration: configuration,
+            rulesDirectory: 'dist/src/',
+            formattersDirectory: 'customFormatters/'
+        };
+
+        var contents = fs.readFileSync(inputFile, 'utf8');
+
+        var ll = new Lint.Linter(inputFile, contents, options);
+        var result : Lint.LintResult = ll.lint();
+
+        var actualFailures : ExpectedFailure[] = JSON.parse(result.output);
+
+        chai.assert.equal(expectedFailures.length, actualFailures.length, 'Wrong # of failures: \n' + JSON.stringify(actualFailures, null, 2));
+
+        expectedFailures.forEach((expected : ExpectedFailure, index: number) : void => {
+            var actual = actualFailures[index];
+            chai.assert.deepEqual(actual, expected);
+        });
+    }
+}
+
+export = TestHelper;

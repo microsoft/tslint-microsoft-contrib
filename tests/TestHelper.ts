e9e67f9901145c4a7c295f190ac7ec247b8f0c05
Daniel Manesku <dmanesku@microsoft.com>
2015-08-03 18:13:33 +0200

    Update TestHelper to take in consideration only relevant fields.
    Adjusting the output for line number and character number to match text
    editor values.

--- tests/TestHelper.ts
+++ tests/TestHelper.ts
@@ -10,13 +10,13 @@ module TestHelper {
     export interface FailurePosition {
         character: number;
         line: number;
-        position: number;
+        position?: number;
     }
     export interface ExpectedFailure {
         ruleName: string;
         name: string;
         failure: string;
-        endPosition: FailurePosition;
+        endPosition?: FailurePosition;
         startPosition: FailurePosition;
     }
 
@@ -44,7 +44,23 @@ module TestHelper {
 
         var result : Lint.LintResult = linter.lint();
 
-        var actualFailures : ExpectedFailure[] = JSON.parse(result.output);
+        var actualFailures: ExpectedFailure[] = JSON.parse(result.output);
+
+        // All the information we need is line and character of start position. For JSON comparison
+        // to work, we will delete the information that we are not interested in from both actual and
+        // expected failures.
+        actualFailures.forEach((actual: ExpectedFailure): void => {
+            delete actual.startPosition.position;
+            delete actual.endPosition;
+            // Editors start counting lines and characters from 1, but tslint does it from 0.
+            // To make thing easier to debug, aling to editor values.
+            actual.startPosition.line = actual.startPosition.line + 1;
+            actual.startPosition.character = actual.startPosition.character + 1;
+        });
+        expectedFailures.forEach((expected: ExpectedFailure): void => {
+            delete expected.startPosition.position;
+            delete expected.endPosition;
+        });
 
         var errorMessage = 'Wrong # of failures: \n' + JSON.stringify(actualFailures, null, 2);
         chai.assert.equal(expectedFailures.length, actualFailures.length, errorMessage);

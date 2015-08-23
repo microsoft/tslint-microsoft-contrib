e4390f34d725065108cd5ad0c905b8f987d5a660
HamletDRC <hamletdrc@gmail.com>
2015-08-23 15:03:22 +0200

    Issue 8 - Fixed false positive in no unused import rule

--- src/noUnusedImportsRule.ts
+++ src/noUnusedImportsRule.ts
@@ -7,30 +7,88 @@ export class Rule extends Lint.Rules.AbstractRule {
         var documentRegistry = ts.createDocumentRegistry();
         var languageServiceHost = Lint.createLanguageServiceHost('file.ts', sourceFile.getFullText());
         var languageService = ts.createLanguageService(languageServiceHost, documentRegistry);
-        return this.applyWithWalker(new NoUnusedImportsWalker(sourceFile, this.getOptions(), languageService));
+
+        var referencedDotImports : { [index: string]: boolean; } = this.getReferencedDotImports(sourceFile, this.getOptions());
+        return this.applyWithWalker(new NoUnusedImportsWalker(sourceFile, this.getOptions(), languageService, referencedDotImports));
+    }
+
+    private getReferencedDotImports(sourceFile : ts.SourceFile, options : Lint.IOptions) : { [index: string]: boolean; } {
+        var gatherImportHandler = new GatherNoRequireImportsWalker(sourceFile, options);
+        this.applyWithWalker(gatherImportHandler);
+        return gatherImportHandler.noRequireReferences;
+    }
+}
+
+class GatherNoRequireImportsWalker extends Lint.RuleWalker {
+
+    public noRequireReferences: { [index: string]: boolean; } = {};
+
+    protected visitImportEqualsDeclaration(node: ts.ImportEqualsDeclaration): void {
+        var moduleReference = node.moduleReference;
+        if (moduleReference.kind === ts.SyntaxKind.QualifiedName) {
+            if ((<any>moduleReference).left != null) {
+                this.gatherReferenceFromImport((<any>moduleReference).left.text);
+            }
+        }
+        super.visitImportEqualsDeclaration(node);
+    }
+
+
+    private gatherReferenceFromImport(qualifiedName : string) : void {
+        if (qualifiedName) {
+            qualifiedName.split('.').forEach((name : string) => {
+                this.noRequireReferences[name] = true;
+            });
+        }
     }
 }
 
 class NoUnusedImportsWalker extends ErrorTolerantWalker {
 
     private languageServices: ts.LanguageService;
+    private noRequireReferences: { [index: string]: boolean; } = {};
 
-    public constructor(sourceFile : ts.SourceFile, options : Lint.IOptions, languageServices : ts.LanguageService) {
+    public constructor(sourceFile : ts.SourceFile,
+                       options : Lint.IOptions,
+                       languageServices : ts.LanguageService,
+                       noRequireReferences: { [index: string]: boolean; }) {
         super(sourceFile, options);
         this.languageServices = languageServices;
+        this.noRequireReferences = noRequireReferences;
     }
 
+
     protected visitImportEqualsDeclaration(node: ts.ImportEqualsDeclaration): void {
-        if (!Lint.hasModifier(node.modifiers, ts.SyntaxKind.ExportKeyword)) {
-            this.validateReferencesForVariable(node.name.text, node.name.getStart());
+        if (!this.hasModifier(node.modifiers, ts.SyntaxKind.ExportKeyword)) {
+            this.validateReferencesForVariable((<any>node).name.text, (<any>node).name.getStart());
         }
         super.visitImportEqualsDeclaration(node);
     }
 
+    private hasModifier(modifiers : ts.ModifiersArray, modifierKind : ts.SyntaxKind) : boolean {
+        if (modifiers == null) {
+            return false;
+        }
+        var result : boolean = false;
+        modifiers.forEach((modifier : ts.Node) : void => {
+            if (modifier.kind === modifierKind) {
+                result = true;
+            }
+        });
+        return result;
+    }
+
+    /**
+     * As described in  https://github.com/palantir/tslint/issues/325,
+     * the language service in typescript 1.4 doesn't seem to search for references in other imports.
+     * This will be fixed, but we can work around it by keeping track for the import references manually.
+     */
     private validateReferencesForVariable(name : string, position : number) {
-        var highlights = this.languageServices.getDocumentHighlights('file.ts', position, ['file.ts']);
-        if (highlights[0].highlightSpans.length <= 1) {
-            this.addFailure(this.createFailure(position, name.length, Rule.FAILURE_STRING + '\'' + name + '\''));
+        var references = this.languageServices.getReferencesAtPosition('file.ts', position);
+        if (references.length <= 1 && !this.noRequireReferences[name]) {
+            var failureString = Rule.FAILURE_STRING + '\'' + name + '\'';
+            var failure = this.createFailure(position, name.length, failureString);
+            this.addFailure(failure);
         }
     }
 }
--- tests/NoUnusedImportsTests.ts
+++ tests/NoUnusedImportsTests.ts
@@ -2,6 +2,7 @@
 /// <reference path="../typings/chai.d.ts" />
 
 /* tslint:disable:quotemark */
+/* tslint:disable:no-multiline-string */
 import TestHelper = require('./TestHelper');
 
 describe('noUnusedImportsRule', () : void => {
@@ -23,24 +24,33 @@ describe('noUnusedImportsRule', () : void => {
         );
     });
 
-    it('FALSE POSITIVE - should not flag imports that are used as other imports', () : void => {
+    it('should flag an unused relative import', () : void => {
+        var ruleName : string = 'no-unused-imports';
+        var inputScript : string = `
+import DM = require("DM");
+import AB = DM.Dependency;
+console.log(DM);`; // AB import is not used!
+
+        TestHelper.assertViolations(ruleName, inputScript, [
+            {
+                "failure": "unused import: 'AB'",
+                "name": "file.ts",
+                "ruleName": "no-unused-imports",
+                "startPosition": { "line": 3, "character": 8 }
+            }
+        ]);
+    });
+
+    it('should not flag imports that are used as other imports', () : void => {
         var ruleName : string = 'no-unused-imports';
         var inputScript : string = `
 import DM = require("DM");
 import AB = DM.Dependency;
 console.log(AB);`;
 
-        TestHelper.assertViolations(
-            ruleName,
-            inputScript, [
-                {
-                    "failure": "unused import: 'DM'",
-                    "name": "file.ts",
-                    "ruleName": "no-unused-imports",
-                    "startPosition": { "line": 2, "character": 8 }
-                }
-            ]);
+        TestHelper.assertViolations(ruleName, inputScript, []);
     });
 
 });
 /* tslint:enable:quotemark */
+/* tslint:enable:no-multiline-string */

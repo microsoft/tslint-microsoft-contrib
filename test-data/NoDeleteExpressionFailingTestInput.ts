7d63f2bfcbe06b28755d3ff6b6af4fc31c52aa33
Daniel Manesku <dmanesku@microsoft.com>
2015-09-08 00:19:29 +0200

    Adjusting NoDeleteExpression test for failing and passing.
    Separating test input files for failing and passing cases.

---
+++ test-data/NoDeleteExpressionFailingTestInput.ts
@@ -0,0 +1,6 @@
+var something: int = 22;
+
+if (something) {
+    var variableForDeletion = 10;
+    delete variableForDeletion;
+}
---
+++ test-data/NoDeleteExpressionPassingTestInput.ts
@@ -0,0 +1,6 @@
+var x = {
+    myProperty: 'sometext'
+};
+delete x.myProperty;
+
+delete this.router.routes[routeName];

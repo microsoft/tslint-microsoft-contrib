550f95426a3ee6db8139dd07e7ecd56a17fe1181
Bogdan Vancea <bovancea@microsoft.com>
2015-10-09 19:22:32 +0200

    [Issue #39] Avoid using Backbone get('X') and set('X', value) outside owning model
    Added rule. Currently the rule fails when the methods called get or set are
    called on an object called this.
    
    Closes #39

---
+++ src/noBackboneGetSetOutsideModelRule.ts
@@ -0,0 +1,37 @@
+/// <reference path="references.ts" />
+
+import ErrorTolerantWalker = require('./ErrorTolerantWalker');
+import AstUtils = require('./AstUtils');
+
+/**
+ * Implementation of the no-backbone-get-set-outside-model rule.
+ *
+ * Currently only makes sure that get and set Backbone methods are called
+ * on the this reference.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+    public static GET_FAILURE_STRING = 'Backbone get() called outside of owning model: ';
+    public static SET_FAILURE_STRING = 'Backbone set() called outside of owning model: ';
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoBackboneGetSetOutsideModelRuleWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoBackboneGetSetOutsideModelRuleWalker extends ErrorTolerantWalker {
+
+    protected visitCallExpression(node: ts.CallExpression): void {
+        if (AstUtils.getFunctionTarget(node) !== 'this') {
+            let functionName: string = AstUtils.getFunctionName(node);
+            if (functionName === 'get' && node.arguments.length === 1 && node.arguments[0].kind === ts.SyntaxKind.StringLiteral) {
+                var msg: string = Rule.GET_FAILURE_STRING + node.getText();
+                this.addFailure(this.createFailure(node.getStart(), node.getEnd(), msg));
+            }
+            if (functionName === 'set' && node.arguments.length === 2 && node.arguments[0].kind === ts.SyntaxKind.StringLiteral) {
+                var msg: string = Rule.SET_FAILURE_STRING + node.getText();
+                this.addFailure(this.createFailure(node.getStart(), node.getEnd(), msg));
+            }
+        }
+        super.visitCallExpression(node);
+    }
+}
---
+++ tests/NoBackboneGetSetOutsideModelRuleTests.ts
@@ -0,0 +1,63 @@
+/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+
+/* tslint:disable:quotemark */
+/* tslint:disable:no-multiline-string */
+
+import TestHelper = require('./TestHelper');
+
+/**
+ * Unit tests.
+ */
+describe('noBackboneGetSetOutsideModelRule', () : void => {
+
+    var ruleName : string = 'no-backbone-get-set-outside-model';
+
+    it('should pass on get and set calls on the this reference', () : void => {
+        var script : string = `
+            var datetime = this.get('timestamp');
+            this.set('modificationdate', datetime);
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should pass on get and set calls with wrong # parameters and wrong parameter types', () : void => {
+        var script : string = `
+            model.get();
+            model.get(someIdentifier);
+            model.get('timestamp', 'someOtherValue');
+
+            model.set();
+            model.set('modificationdate');
+            model.set('modificationdate', value1, value2);
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should fail on get and set on an object different than this', () : void => {
+        var script : string = `
+            var datetime = model.get('timestamp');
+            model.set('modificationdate', datetime);
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Backbone get() called outside of owning model: model.get('timestamp')",
+                "name": "file.ts",
+                "ruleName": "no-backbone-get-set-outside-model",
+                "startPosition": { "character": 28, "line": 2 }
+            },
+            {
+                "failure": "Backbone set() called outside of owning model: model.set('modificationdate', datetime)",
+                "name": "file.ts",
+                "ruleName": "no-backbone-get-set-outside-model",
+                "startPosition": { "character": 13, "line": 3 }
+            }
+        ]);
+    });
+
+});
+/* tslint:enable:quotemark */
+/* tslint:enable:no-multiline-string */

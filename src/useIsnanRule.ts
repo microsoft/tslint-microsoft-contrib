6f640f33704add0ff20be3545916d26835cadacf
HamletDRC <hamletdrc@gmail.com>
2015-10-20 20:36:46 +0200

    [Issue #55] new rule: invalid comparison against NaN
    closes #55

---
+++ src/useIsnanRule.ts
@@ -0,0 +1,27 @@
+import SyntaxKind = require('./utils/SyntaxKind');
+import ErrorTolerantWalker = require('./utils/ErrorTolerantWalker');
+
+/**
+ * Implementation of the use-isnan rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = 'Found an invalid comparison for NaN: ';
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new UseIsnanRuleWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class UseIsnanRuleWalker extends ErrorTolerantWalker {
+
+    protected visitBinaryExpression(node: ts.BinaryExpression): void {
+        if (this.isExpressionNaN(node.left) || this.isExpressionNaN(node.right)) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING + node.getText()));
+        }
+        super.visitBinaryExpression(node);
+    }
+
+    private isExpressionNaN(node: ts.Node) {
+        return node.kind === SyntaxKind.current().Identifier && node.getText() === 'NaN';
+    }
+}
---
+++ tests/UseIsnanRuleTests.ts
@@ -0,0 +1,99 @@
+/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+
+/* tslint:disable:quotemark */
+/* tslint:disable:no-multiline-string */
+
+import TestHelper = require('./TestHelper');
+
+/**
+ * Unit tests.
+ */
+describe('useIsnanRule', () : void => {
+
+    var ruleName : string = 'use-isnan';
+
+    it('should pass on xxx', () : void => {
+        var script : string = `
+        if (isNaN(NaN)) { }
+        if (isNaN(something)) { }
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should fail on equality NaN', () : void => {
+        var script : string = `
+        if (foo == NaN) {  }
+        if (NaN === foo) {  }
+        if (foo != NaN) {  }
+        if (NaN !== foo) {  }
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Found an invalid comparison for NaN: foo == NaN",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 2 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: NaN === foo",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 3 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: foo != NaN",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 4 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: NaN !== foo",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 5 }
+            }
+        ]);
+    });
+
+    it('should fail on other binary expressions', () : void => {
+        var script : string = `
+        if (foo > NaN) { }
+        if (NaN >= foo) { }
+        if (foo < NaN) { }
+        if (NaN <= foo) { }
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Found an invalid comparison for NaN: foo > NaN",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 2 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: NaN >= foo",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 3 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: foo < NaN",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 4 }
+            },
+            {
+                "failure": "Found an invalid comparison for NaN: NaN <= foo",
+                "name": "file.ts",
+                "ruleName": "use-isnan",
+                "startPosition": { "character": 13, "line": 5 }
+            }
+        ]);
+    });
+
+});
+/* tslint:enable:quotemark */
+/* tslint:enable:no-multiline-string */

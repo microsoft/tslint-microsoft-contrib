9632adaee05c1cf826f814d450d6ded70b8e43c0
HamletDRC <hamletdrc@gmail.com>
2016-01-15 19:56:22 +0100

    [Issue #75] added Array constructor to the prefer-array-literal-rule
    closes #75

--- src/preferArrayLiteralRule.ts
+++ src/preferArrayLiteralRule.ts
@@ -3,12 +3,13 @@ import * as Lint from 'tslint/lib/lint';
 
 import SyntaxKind = require('./utils/SyntaxKind');
 import ErrorTolerantWalker = require('./utils/ErrorTolerantWalker');
-
+import AstUtils = require('./utils/AstUtils');
 /**
  * Implementation of the prefer-array-literal rule.
  */
 export class Rule extends Lint.Rules.AbstractRule {
-    public static FAILURE_STRING = 'Replace generic-typed Array with array literal: ';
+    public static GENERICS_FAILURE_STRING = 'Replace generic-typed Array with array literal: ';
+    public static CONSTRUCTOR_FAILURE_STRING = 'Replace Array constructor with an array literal: ';
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         return this.applyWithWalker(new NoGenericArrayWalker(sourceFile, this.getOptions()));
@@ -21,11 +22,21 @@ class NoGenericArrayWalker extends ErrorTolerantWalker {
         if (node.kind === SyntaxKind.current().TypeReference) {
             let ref : ts.TypeReferenceNode = <ts.TypeReferenceNode>node;
             if ((<ts.Identifier>ref.typeName).text === 'Array') {
-                var failureString = Rule.FAILURE_STRING + node.getText();
+                var failureString = Rule.GENERICS_FAILURE_STRING + node.getText();
                 var failure = this.createFailure(node.getStart(), node.getWidth(), failureString);
                 this.addFailure(failure);
             }
         }
         super.visitNode(node);
+    }
+
+
+    protected visitNewExpression(node: ts.NewExpression): void {
+        var functionName  = AstUtils.getFunctionName(node);
+        if (functionName === 'Array') {
+            var failureString = Rule.CONSTRUCTOR_FAILURE_STRING + node.getText();
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), failureString));
+        }
+        super.visitNewExpression(node);
     }
 }

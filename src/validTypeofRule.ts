7f6c5ef0855fe863bd484561ab65dd631974c5ab
reversi.fun <junichi.kose.ax@hitachi-systems.com>
2016-01-21 19:58:24 +0900

    repare too slow ValidTypeofRuleWalker

--- src/validTypeofRule.ts
+++ src/validTypeofRule.ts
@@ -22,10 +22,16 @@ export class Rule extends Lint.Rules.AbstractRule {
 class ValidTypeofRuleWalker extends ErrorTolerantWalker {
 
     protected visitBinaryExpression(node: ts.BinaryExpression): void {
-        if (node.left.kind === SyntaxKind.current().TypeOfExpression && node.right.kind === SyntaxKind.current().StringLiteral) {
-            this.validateTypeOf(<ts.StringLiteral>node.right);
-        } else if (node.right.kind === SyntaxKind.current().TypeOfExpression && node.left.kind === SyntaxKind.current().StringLiteral) {
-            this.validateTypeOf(<ts.StringLiteral>node.left);
+        if (node.operatorToken.kind === SyntaxKind.current().EqualsEqualsEqualsToken
+            || node.operatorToken.kind === SyntaxKind.current().EqualsEqualsToken
+            || node.operatorToken.kind === SyntaxKind.current().ExclamationEqualsEqualsToken
+            || node.operatorToken.kind === SyntaxKind.current().ExclamationEqualsToken
+        ) {
+            if (node.left.kind === SyntaxKind.current().TypeOfExpression && node.right.kind === SyntaxKind.current().StringLiteral) {
+                this.validateTypeOf(<ts.StringLiteral>node.right);
+            } else if (node.right.kind === SyntaxKind.current().TypeOfExpression && node.left.kind === SyntaxKind.current().StringLiteral) {
+                this.validateTypeOf(<ts.StringLiteral>node.left);
+            }
         }
         super.visitBinaryExpression(node);
     }
@@ -41,7 +47,7 @@ class ValidTypeofRuleWalker extends ErrorTolerantWalker {
     private getClosestTerm(term: string): string {
         var closestMatch: number = 99999999;
         return Utils.reduce(Rule.VALID_TERMS, (closestTerm: string, thisTerm: string) : string => {
-            var distance = this.hammingDistance(term, thisTerm);
+            var distance = this.levenshteinDistance(term, thisTerm);
             if (distance < closestMatch) {
                 closestMatch = distance;
                 closestTerm = thisTerm;
@@ -67,4 +73,18 @@ class ValidTypeofRuleWalker extends ErrorTolerantWalker {
                 this. hammingDistance(source.substr(1), target.substr(1)) + (source[0] !== target[0] ? 1 : 0)
             ) + 1;
     }
+    private levenshteinDistance(a: string, b: string): number {
+        'use strict';
+        if (a === b) { return 0; }
+        if (!a.length || !b.length) { return b.length || a.length; }
+        var len1 = a.length + 1, len2 = b.length + 1, I = 0, i = 0, d = [[0]], c, j, J;
+        while (++i < len2) { d[0][i] = i; };
+        i = 0;
+        while (++i < len1) {
+            J = j = 0; c = a[I]; d[i] = [i];
+            while (++j < len2) { d[i][j] = Math.min(d[I][j] + 1, d[i][J] + 1, d[I][J] + ((c !== b[J]) ? 1 : 0)); ++J; };
+            ++I;
+        }
+        return d[len1 - 1][len2 - 1];
+    };
 }

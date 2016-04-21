a96fe4eed6da64467fc5d6577cf4d7a5c236f783
HamletDRC <hamletdrc@gmail.com>
2016-04-21 13:59:54 +0200

    [Issue #7] added the 'no-document-domain' rule
    Closes #7

---
+++ src/noDocumentDomainRule.ts
@@ -0,0 +1,39 @@
+import * as ts from 'typescript';
+import * as Lint from 'tslint/lib/lint';
+
+import ErrorTolerantWalker = require('./utils/ErrorTolerantWalker');
+import SyntaxKind = require('./utils/SyntaxKind');
+
+/**
+ * Implementation of the no-document-domain rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = 'Forbidden write to document.domain: ';
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoDocumentDomainRuleWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoDocumentDomainRuleWalker extends ErrorTolerantWalker {
+
+    protected visitBinaryExpression(node: ts.BinaryExpression): void {
+        if (node.operatorToken.getText() === '='
+            && node.left.kind === SyntaxKind.current().PropertyAccessExpression
+            && this.isDocumentDomainProperty(<ts.PropertyAccessExpression>node.left)) {
+
+            let msg: string = Rule.FAILURE_STRING + node.getFullText().trim();
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), msg));
+        }
+        super.visitBinaryExpression(node);
+    }
+
+    private isDocumentDomainProperty(node: ts.PropertyAccessExpression): boolean {
+        if (node.name.text !== 'domain') {
+            return false;
+        }
+        return node.expression.getText() === 'document'
+            || node.expression.getText() === 'window.document';
+
+    }
+}
---
+++ tests/NoDocumentDomainRuleTests.ts
@@ -0,0 +1,86 @@
+/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+
+/* tslint:disable:quotemark */
+/* tslint:disable:no-multiline-string */
+
+import TestHelper = require('./TestHelper');
+
+/**
+ * Unit tests.
+ */
+describe('noDocumentDomainRule', () : void => {
+
+    var ruleName : string = 'no-document-domain';
+
+    it('should pass when not setting a value to document.domain', () : void => {
+        var script : string = `
+            console.log(document.domain);
+            console.log(document.domain.value);
+            let x = document.domain;
+            let y = document.domain.value;
+
+            document.domain.value = 'some value';
+            model.domain = 'some model value';
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should fail on assigning constant', () : void => {
+        var script : string = `
+            document.domain = 'some value';
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Forbidden write to document.domain: document.domain = 'some value'",
+                "name": "file.ts",
+                "ruleName": "no-document-domain",
+                "startPosition": { "character": 13, "line": 2 }
+            }
+        ]);
+    });
+
+    it('should fail on assigning variable', () : void => {
+        var script : string = `
+            document.domain = someValue;
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Forbidden write to document.domain: document.domain = someValue",
+                "name": "file.ts",
+                "ruleName": "no-document-domain",
+                "startPosition": { "character": 13, "line": 2 }
+            }
+        ]);
+    });
+
+    it('should fail on assigning variable on window', () : void => {
+        var script : string = `
+            window.document.domain = someValue;
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Forbidden write to document.domain: window.document.domain = someValue",
+                "name": "file.ts",
+                "ruleName": "no-document-domain",
+                "startPosition": { "character": 13, "line": 2 }
+            }
+        ]);
+    });
+
+    it('should pass when document is aliased because the type checker is so weak', () : void => {
+        var script : string = `
+            let doc: Document = document;
+            doc.domain = 'some value';
+        `;
+
+        TestHelper.assertViolations(ruleName, script, []);
+    });
+
+});
+/* tslint:enable:quotemark */
+/* tslint:enable:no-multiline-string */

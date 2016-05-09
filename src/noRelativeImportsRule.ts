aca8f4f6121c58125c5be140acb7500c1429b3d4
HamletDRC <hamletdrc@gmail.com>
2016-05-09 19:05:37 +0200

    [issue #124] new rule: no-relative-imports
    closes #124

---
+++ src/noRelativeImportsRule.ts
@@ -0,0 +1,45 @@
+import * as ts from 'typescript';
+import * as Lint from 'tslint/lib/lint';
+
+import ErrorTolerantWalker = require('./utils/ErrorTolerantWalker');
+import SyntaxKind = require('./utils/SyntaxKind');
+
+const FAILURE_STRING_EXT: string = 'External module is being loaded from a relative path. Please use an absolute path: ';
+const FAILURE_STRING_IMPORT: string = 'Imported module is being loaded from a relative path. Please use an absolute path: ';
+
+/**
+ * Implementation of the no-relative-imports rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoRelativeImportsRuleWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoRelativeImportsRuleWalker extends ErrorTolerantWalker {
+
+    protected visitNode(node: ts.Node): void {
+        if (node.kind === SyntaxKind.current().ExternalModuleReference) {
+            let moduleExpression: ts.Expression = (<ts.ExternalModuleReference>node).expression;
+            if (!this.isModuleExpressionValid(moduleExpression)) {
+                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), FAILURE_STRING_EXT + node.getText()));
+            }
+        } else if (node.kind === SyntaxKind.current().ImportDeclaration) {
+            let moduleExpression: ts.Expression = (<ts.ImportDeclaration>node).moduleSpecifier;
+            if (!this.isModuleExpressionValid(moduleExpression)) {
+                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), FAILURE_STRING_IMPORT + node.getText()));
+            }
+        }
+        super.visitNode(node);
+    }
+
+    private isModuleExpressionValid(expression: ts.Expression): boolean {
+        if (expression.kind === SyntaxKind.current().StringLiteral) {
+            let moduleName: ts.StringLiteral = <ts.StringLiteral>expression;
+            if (moduleName.text[0] === '.') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ tests/NoRelativeImportsRuleTests.ts
@@ -0,0 +1,82 @@
+/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+
+/* tslint:disable:quotemark */
+/* tslint:disable:no-multiline-string */
+
+import TestHelper = require('./TestHelper');
+
+/**
+ * Unit tests.
+ */
+describe('noRelativeImportsRule', () : void => {
+
+    var ruleName : string = 'no-relative-imports';
+
+    it('should pass on absolute path require imports', () : void => {
+        var script : string = `
+            import App = require('App');
+            import App = require('common/App');
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should pass on absolute path ES6 imports', () : void => {
+        var script : string = `
+            ﻿import OfficeApp from 'OfficeApp';
+            ﻿import OfficeApp from 'common/OfficeApp';
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [ ]);
+    });
+
+    it('should fail on relative path require import', () : void => {
+        var script : string = `
+            import App = require('./App');
+            import App = require('../common/App');
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "External module is being loaded from a relative path. Please use an absolute path: require('./App')",
+                "name": "file.ts",
+                "ruleName": "no-relative-imports",
+                "startPosition": { "character": 26, "line": 2 }
+            },
+            {
+                "failure": "External module is being loaded from a relative path. Please use an absolute path: require('../common/App')",
+                "name": "file.ts",
+                "ruleName": "no-relative-imports",
+                "startPosition": { "character": 26, "line": 3 }
+            }
+        ]);
+    });
+
+    it('should fail on relative path ES6 import', () : void => {
+        var script : string = `
+            ﻿import OfficeApp from './OfficeApp';
+            ﻿import OfficeApp from '../common/OfficeApp';
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Imported module is being loaded from a relative path. " +
+                    "Please use an absolute path: import OfficeApp from './OfficeApp';",
+                "name": "file.ts",
+                "ruleName": "no-relative-imports",
+                "startPosition": { "character": 14, "line": 2 }
+            },
+            {
+                "failure": "Imported module is being loaded from a relative path. " +
+                    "Please use an absolute path: import OfficeApp from '../common/OfficeApp';",
+                "name": "file.ts",
+                "ruleName": "no-relative-imports",
+                "startPosition": { "character": 14, "line": 3 }
+            }
+        ]);
+    });
+
+});
+/* tslint:enable:quotemark */
+/* tslint:enable:no-multiline-string */

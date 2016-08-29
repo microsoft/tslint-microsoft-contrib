22f95a8e1e1a8d51c7dc86bf79e76599f4083baf
Cosmin Cojocar <cosmin.cojocar@protonmail.ch>
2016-08-29 17:57:03 +0200

    Title content should be concise (less than 60 chars)

--- tests/ReactA11yTitlesRuleTests.ts
+++ tests/ReactA11yTitlesRuleTests.ts
@@ -53,4 +53,22 @@ describe('reactA11yTitlesRule', () : void => {
             }
         ]);
     });
+
+    it.only('should fail on longer than 60 charactes title', () : void => {
+        let title: string = Array(61).join("a")
+        const script : string = `
+            import React = require('react');
+
+            const title = <title>${title}</title>;
+        `;
+
+        TestHelper.assertViolations(ruleName, script, [
+            {
+                "failure": "Title length must not be longer than 60 characters: aaaaaaaaaaaaaaaaaa...",
+                "name": "file.tsx",
+                "ruleName": "react-a11y-titles",
+                "startPosition": { "character": 27, "line": 4 }
+            }
+        ]);
+    });
 });

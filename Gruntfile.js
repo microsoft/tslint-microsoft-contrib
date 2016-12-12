9a4a708b14f98a3c85d755319bf9d60741bfca59
Akinmade Bond <lovedammy77@gmail.com>
2016-12-12 18:09:09 +0100

    Skip rules that have already been disabled in the config
    tslint 4 now accepts this rules as an array, and since they aren't
    already enabled in the project, they should be skipped so the tests
    can pass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -335,7 +335,12 @@ module.exports = function(grunt) {
             'no-inferrable-types': true,
             'only-arrow-functions': true,
             'ordered-imports': true,
-            'typedef-whitespace': true
+            'typedef-whitespace': true,
+            'array-type': true,
+            'completed-docs': true,
+            'cyclomatic-complexity': true,
+            'file-header': true,
+            'max-classes-per-file': true
         };
         var errors = [];
         getAllRuleNames().forEach(function(ruleName) {

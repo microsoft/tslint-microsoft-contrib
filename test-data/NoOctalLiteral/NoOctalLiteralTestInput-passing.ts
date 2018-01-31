7196a266c28970551c77af7a8617086bf86f9870
Ian Clanton-Thuon <ianc@microsoft.com>
2018-01-31 13:51:28 -0800

    #412 Fixing an edge case in the no-octal-literal rule where numbers preceeded by an escaped backslash would fail the rule.
    Fixing an issue where the no-octal-rule wouldn't handle template strings or newlines inside strings.
    
    Fixing test numbering.
    
    Fixing duplicate variable declaration.

--- test-data/NoOctalLiteral/NoOctalLiteralTestInput-passing.ts
+++ test-data/NoOctalLiteral/NoOctalLiteralTestInput-passing.ts
@@ -1,4 +1,16 @@
 function demoScriptPass1() {
-    var x = "Sample text \xB2";
-    var y = "Sample text \0111"; // longer than octal
+    var w1 = "Sample text \xB2";
+    var x1 = "Sample text \0111"; // longer than octal
+    var y1 = "Sample text \\1";
+    var z1 = "Sample text \\\\1";
+
+    var w2 = 'Sample text \xB2';
+    var x2 = 'Sample text \0111'; // longer than octal
+    var y2 = 'Sample text \\1';
+    var z2 = 'Sample text \\\\1';
+
+    var w3 = `Sample text \xB2`;
+    var x3 = `Sample text \0111`; // longer than octal
+    var y3 = `Sample text \\1`;
+    var z3 = `Sample text \\\\1`;
 };

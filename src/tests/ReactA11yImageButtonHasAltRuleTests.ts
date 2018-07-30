ba60106f5fa9087dde1c4801bf5e0adee85662d4
Matt Fedderly <mfedderly@palantir.com>
2018-07-30 18:23:02 -0400

    Fix react-a11y-image-button-has-alt failure

--- src/tests/ReactA11yImageButtonHasAltRuleTests.ts
+++ src/tests/ReactA11yImageButtonHasAltRuleTests.ts
@@ -27,6 +27,16 @@ describe('reactA11yImageButtonHasAlt', (): void => {
 
             TestHelper.assertNoViolation(ruleName, script);
         });
+
+        it("when input element has an expression for its type.", (): void => {
+            const script: string = `
+                import React = require('react');
+                const type: string = "text";
+                const a = <input type={type} />;
+            `;
+
+            TestHelper.assertNoViolation(ruleName, script);
+        });
     });
 
     describe('should fail', (): void => {

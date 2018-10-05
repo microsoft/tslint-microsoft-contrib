0ca679b1f6c25d2a82862b8a3aa1441c62c8afec
Andrii Dieiev <illusionmh@gmail.com>
2018-10-05 03:18:03 +0300

    Allow same folder imports (fixes #493) (#508)
    * Clean up. Fix lint error and unexpected chars.
    
    * Add allow-siblings option to no-relative-imports rule (fixes #493)
    
    * Check parent reference only at the import start

--- src/noRelativeImportsRule.ts
+++ src/noRelativeImportsRule.ts
@@ -4,8 +4,14 @@ import * as Lint from 'tslint';
 import {ErrorTolerantWalker} from './utils/ErrorTolerantWalker';
 import {ExtendedMetadata} from './utils/ExtendedMetadata';
 
+const OPTION_ALLOW_SIBLINGS = 'allow-siblings';
+
 const FAILURE_STRING_EXT: string = 'External module is being loaded from a relative path. Please use an absolute path: ';
 const FAILURE_STRING_IMPORT: string = 'Imported module is being loaded from a relative path. Please use an absolute path: ';
+const FAILURE_STRING_EXT_SIBLINGS: string =
+    'External module path starts with reference to parent directory. Please use an absolute path or sibling files/folders: ';
+const FAILURE_STRING_IMPORT_SIBLINGS: string =
+    'Imported module path starts with reference to parent directory. Please use an absolute path or sibling files/folders: ';
 
 /**
  * Implementation of the no-relative-imports rule.
@@ -16,9 +22,18 @@ export class Rule extends Lint.Rules.AbstractRule {
         ruleName: 'no-relative-imports',
         type: 'maintainability',
         description: 'Do not use relative paths when importing external modules or ES6 import declarations',
-        options: null,
-        optionsDescription: '',
-        typescriptOnly: true,
+        options: {
+            type: 'array',
+            items: {
+              type: 'string',
+              enum: [OPTION_ALLOW_SIBLINGS]
+            },
+            minLength: 0,
+            maxLength: 1
+          },
+        optionsDescription: `One argument may be optionally provided: \n\n' +
+            '* \`${OPTION_ALLOW_SIBLINGS}\` allows relative imports for files in the same or nested folders.`,
+        typescriptOnly: false,
         issueClass: 'Ignored',
         issueType: 'Warning',
         severity: 'Low',
@@ -33,16 +48,26 @@ export class Rule extends Lint.Rules.AbstractRule {
 }
 
 class NoRelativeImportsRuleWalker extends ErrorTolerantWalker {
+    private allowSiblings: boolean;
+
+    constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
+        super(sourceFile, options);
+
+        this.allowSiblings = options.ruleArguments.indexOf(OPTION_ALLOW_SIBLINGS) > -1;
+    }
+
     protected visitNode(node: ts.Node): void {
         if (node.kind === ts.SyntaxKind.ExternalModuleReference) {
             const moduleExpression: ts.Expression = (<ts.ExternalModuleReference>node).expression;
             if (!this.isModuleExpressionValid(moduleExpression)) {
-                this.addFailureAt(node.getStart(), node.getWidth(), FAILURE_STRING_EXT + node.getText());
+                const failureStart = this.allowSiblings ? FAILURE_STRING_EXT_SIBLINGS : FAILURE_STRING_EXT;
+                this.addFailureAt(node.getStart(), node.getWidth(), failureStart + node.getText());
             }
         } else if (node.kind === ts.SyntaxKind.ImportDeclaration) {
             const moduleExpression: ts.Expression = (<ts.ImportDeclaration>node).moduleSpecifier;
             if (!this.isModuleExpressionValid(moduleExpression)) {
-                this.addFailureAt(node.getStart(), node.getWidth(), FAILURE_STRING_IMPORT + node.getText());
+                const failureStart = this.allowSiblings ? FAILURE_STRING_IMPORT_SIBLINGS : FAILURE_STRING_IMPORT;
+                this.addFailureAt(node.getStart(), node.getWidth(), failureStart + node.getText());
             }
         }
         super.visitNode(node);
@@ -51,7 +76,14 @@ class NoRelativeImportsRuleWalker extends ErrorTolerantWalker {
     private isModuleExpressionValid(expression: ts.Expression): boolean {
         if (expression.kind === ts.SyntaxKind.StringLiteral) {
             const moduleName: ts.StringLiteral = <ts.StringLiteral>expression;
-            if (moduleName.text[0] === '.') {
+
+            // when no siblings allowed path cannot start with '.' (relative)
+            if (!this.allowSiblings && moduleName.text[0] === '.') {
+                return false;
+            }
+
+            // when siblings allowed path cannot start '..' (reference to parrent directory)
+            if (this.allowSiblings && moduleName.text.indexOf('..') === 0) {
                 return false;
             }
         }
--- src/tests/NoRelativeImportsRuleTests.ts
+++ src/tests/NoRelativeImportsRuleTests.ts
@@ -7,69 +7,163 @@ import {TestHelper} from './TestHelper';
 describe('noRelativeImportsRule', () : void => {
     const ruleName : string = 'no-relative-imports';
 
-    it('should pass on absolute path require imports', () : void => {
-        const script : string = `
-            import App = require('App');
-            import App = require('common/App');
-        `;
+    describe('no options', () : void => {
+        it('should pass on absolute path require imports', () : void => {
+            const script : string = `
+                import App = require('App');
+                import App = require('common/App');
+            `;
 
-        TestHelper.assertViolations(ruleName, script, [ ]);
+            TestHelper.assertViolations(ruleName, script, [ ]);
+        });
+
+        it('should pass on absolute path ES6 imports', () : void => {
+            const script : string = `
+                import OfficeApp from 'OfficeApp';
+                import OfficeApp from 'common/OfficeApp';
+            `;
+
+            TestHelper.assertViolations(ruleName, script, [ ]);
+        });
+
+        it('should fail on relative path require import', () : void => {
+            const script : string = `
+                import App = require('./App');
+                import App = require('./common/App');
+                import App = require('../common/App');
+                import App = require('./../common/App');
+            `;
+
+            TestHelper.assertViolations(ruleName, script, [
+                {
+                    failure: 'External module is being loaded from a relative path. ' +
+                        'Please use an absolute path: require(\'./App\')',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 30, line: 2 }
+                },
+                {
+                    failure: 'External module is being loaded from a relative path. ' +
+                        'Please use an absolute path: require(\'./common/App\')',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 30, line: 3 }
+                },
+                {
+                    failure: 'External module is being loaded from a relative path. ' +
+                        'Please use an absolute path: require(\'../common/App\')',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 30, line: 4 }
+                },
+                {
+                    failure: 'External module is being loaded from a relative path. ' +
+                        'Please use an absolute path: require(\'./../common/App\')',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 30, line: 5 }
+                }
+            ]);
+        });
+
+        it('should fail on relative path ES6 import', () : void => {
+            const script : string = `
+                import OfficeApp from './OfficeApp';
+                import OfficeApp from './common/OfficeApp';
+                import OfficeApp from '../common/OfficeApp';
+                import OfficeApp from './../common/OfficeApp';
+            `;
+
+            TestHelper.assertViolations(ruleName, script, [
+                {
+                    failure: 'Imported module is being loaded from a relative path. ' +
+                        'Please use an absolute path: import OfficeApp from \'./OfficeApp\';',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 17, line: 2 }
+                },
+                {
+                    failure: 'Imported module is being loaded from a relative path. ' +
+                        'Please use an absolute path: import OfficeApp from \'./common/OfficeApp\';',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 17, line: 3 }
+                },
+                {
+                    failure: 'Imported module is being loaded from a relative path. ' +
+                        'Please use an absolute path: import OfficeApp from \'../common/OfficeApp\';',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 17, line: 4 }
+                },
+                {
+                    failure: 'Imported module is being loaded from a relative path. ' +
+                        'Please use an absolute path: import OfficeApp from \'./../common/OfficeApp\';',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 17, line: 5 }
+                }
+            ]);
+        });
     });
 
-    it('should pass on absolute path ES6 imports', () : void => {
-        const script : string = `
-            ﻿import OfficeApp from 'OfficeApp';
-            ﻿import OfficeApp from 'common/OfficeApp';
-        `;
+    describe('allow-siblings enabled', (): void => {
+        const options = ['allow-siblings'];
 
-        TestHelper.assertViolations(ruleName, script, [ ]);
+        it('should pass on absolute path require imports when siblings allowed', () : void => {
+            const script : string = `
+                import App = require('App');
+                import App = require('common/App');
+            `;
+            TestHelper.assertViolationsWithOptions(ruleName, options, script, [ ]);
+        });
+
+        it('should pass on absolute path ES6 imports when siblings allowed', () : void => {
+            const script : string = `
+                import OfficeApp from 'OfficeApp';
+                import OfficeApp from 'common/OfficeApp';
+            `;
+
+            TestHelper.assertViolationsWithOptions(ruleName, options, script, [ ]);
+        });
+
+        it('should fail only when path starts with \'..\' in require import when siblings allowed', () : void => {
+            const script : string = `
+                import App = require('./App');
+                import App = require('./common/App');
+                import App = require('../common/App');
+                import App = require('./../common/App');
+            `;
+
+            TestHelper.assertViolationsWithOptions(ruleName, options, script, [
+                {
+                    failure: 'External module path starts with reference to parent directory. ' +
+                        'Please use an absolute path or sibling files/folders: require(\'../common/App\')',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 30, line: 4 }
+                }
+            ]);
+        });
+
+        it('should fail only when path starts with \'..\' in ES6 import when siblings allowed', () : void => {
+            const script : string = `
+                import OfficeApp from './OfficeApp';
+                import OfficeApp from './common/OfficeApp';
+                import OfficeApp from '../common/OfficeApp';
+                import OfficeApp from './../common/OfficeApp';
+            `;
+
+            TestHelper.assertViolationsWithOptions(ruleName, options, script, [
+                {
+                    failure: 'Imported module path starts with reference to parent directory. ' +
+                        'Please use an absolute path or sibling files/folders: import OfficeApp from \'../common/OfficeApp\';',
+                    name: 'file.ts',
+                    ruleName: 'no-relative-imports',
+                    startPosition: { character: 17, line: 4 }
+                }
+            ]);
+        });
     });
-
-    it('should fail on relative path require import', () : void => {
-        const script : string = `
-            import App = require('./App');
-            import App = require('../common/App');
-        `;
-
-        TestHelper.assertViolations(ruleName, script, [
-            {
-                "failure": "External module is being loaded from a relative path. Please use an absolute path: require('./App')",
-                "name": "file.ts",
-                "ruleName": "no-relative-imports",
-                "startPosition": { "character": 26, "line": 2 }
-            },
-            {
-                "failure": "External module is being loaded from a relative path. Please use an absolute path: require('../common/App')",
-                "name": "file.ts",
-                "ruleName": "no-relative-imports",
-                "startPosition": { "character": 26, "line": 3 }
-            }
-        ]);
-    });
-
-    it('should fail on relative path ES6 import', () : void => {
-        const script : string = `
-            ﻿import OfficeApp from './OfficeApp';
-            ﻿import OfficeApp from '../common/OfficeApp';
-        `;
-
-        TestHelper.assertViolations(ruleName, script, [
-            {
-                "failure": "Imported module is being loaded from a relative path. " +
-                    "Please use an absolute path: import OfficeApp from './OfficeApp';",
-                "name": "file.ts",
-                "ruleName": "no-relative-imports",
-                "startPosition": { "character": 14, "line": 2 }
-            },
-            {
-                "failure": "Imported module is being loaded from a relative path. " +
-                    "Please use an absolute path: import OfficeApp from '../common/OfficeApp';",
-                "name": "file.ts",
-                "ruleName": "no-relative-imports",
-                "startPosition": { "character": 14, "line": 3 }
-            }
-        ]);
-    });
-
 });
 /* tslint:enable:no-irregular-whitespace */

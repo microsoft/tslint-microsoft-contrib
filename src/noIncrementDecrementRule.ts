38c8f1d93e25d8e1d2d9132fae57d5d53f22e17a
Retsam <Retsam@users.noreply.github.com>
2018-11-05 18:56:16 -0500

    Fix #362: Add an `allow-for-loop` option to `no-increment-decrement` (#542)
    * Add an option to noIncrementDecrementRule to ignore for loop headers
    
    * Disallow incrementors and decrementors in parts of the for loop header, other than the incrementor

--- src/noIncrementDecrementRule.ts
+++ src/noIncrementDecrementRule.ts
@@ -3,13 +3,27 @@ import * as Lint from 'tslint';
 
 import { ExtendedMetadata } from './utils/ExtendedMetadata';
 
+const OPTION_ALLOW_FOR_LOOPS = 'allow-for-loops';
+
+/**
+ * Implementation of the no-increment-decrement rule.
+ */
 export class Rule extends Lint.Rules.AbstractRule {
     public static metadata: ExtendedMetadata = {
         ruleName: 'no-increment-decrement',
         type: 'maintainability',
         description: 'Avoid use of increment and decrement operators particularly as part of complicated expressions',
-        options: null, // tslint:disable-line:no-null-keyword
-        optionsDescription: '',
+        options: {
+            type: 'array',
+            items: {
+                type: 'string',
+                enum: [OPTION_ALLOW_FOR_LOOPS]
+            },
+            minLength: 0,
+            maxLength: 1
+        },
+        optionsDescription: `One argument may be optionally provided: \n\n' +
+        '* \`${OPTION_ALLOW_FOR_LOOPS}\` allows increments and decrement operators to be used in for loop headers.`,
         typescriptOnly: true,
         issueClass: 'Non-SDL',
         issueType: 'Warning',
@@ -25,6 +39,31 @@ export class Rule extends Lint.Rules.AbstractRule {
 }
 
 class NoIncrementDecrementWalker extends Lint.RuleWalker {
+    private readonly allowForLoops: boolean;
+
+    constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
+        super(sourceFile, options);
+
+        this.allowForLoops = options.ruleArguments.indexOf(OPTION_ALLOW_FOR_LOOPS) > -1;
+    }
+
+    protected visitForStatement(node: ts.ForStatement): void {
+        if (this.allowForLoops) {
+            // If for loops are allowed to contain increment and decrement,
+            // check everything except the incrementor
+            super.visitNode(node.statement);
+            if (node.initializer) {
+                super.visitNode(node.initializer);
+            }
+            if (node.condition) {
+                super.visitNode(node.condition);
+            }
+        } else {
+            // Otherwise check the node
+            super.visitForStatement(node);
+        }
+    }
+
     protected visitPostfixUnaryExpression(node: ts.PostfixUnaryExpression): void {
         this.validateUnaryExpression(node);
         super.visitPostfixUnaryExpression(node);

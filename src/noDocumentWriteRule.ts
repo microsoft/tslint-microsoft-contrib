96c5bbcb6a5593a17e02135a82e79d78c03f39ea
drexler <andrew.quartey@gmail.com>
2018-12-30 14:40:28 -0500

    convert no-document-write rule to use a walk function

--- src/noDocumentWriteRule.ts
+++ src/noDocumentWriteRule.ts
@@ -1,5 +1,6 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
+import * as tsutils from 'tsutils';
 
 import { AstUtils } from './utils/AstUtils';
 import { ExtendedMetadata } from './utils/ExtendedMetadata';
@@ -24,24 +25,28 @@ export class Rule extends Lint.Rules.AbstractRule {
     public static WRITELN_FAILURE: string = 'Forbidden call to document.writeln';
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new NoDocumentWriteWalker(sourceFile, this.getOptions()));
+        return this.applyWithFunction(sourceFile, walk);
     }
 }
 
-class NoDocumentWriteWalker extends Lint.RuleWalker {
-    protected visitCallExpression(node: ts.CallExpression) {
-        const functionTarget = AstUtils.getFunctionTarget(node);
-        if (functionTarget === 'document' || functionTarget === 'window.document') {
-            if (node.arguments.length === 1) {
-                const functionName: string = AstUtils.getFunctionName(node);
-                if (functionName === 'write') {
-                    this.addFailureAt(node.getStart(), node.getWidth(), Rule.WRITE_FAILURE);
-                } else if (functionName === 'writeln') {
-                    this.addFailureAt(node.getStart(), node.getWidth(), Rule.WRITELN_FAILURE);
+function walk(ctx: Lint.WalkContext<void>) {
+    function cb(node: ts.Node): void {
+        if (tsutils.isCallExpression(node)) {
+            const functionTarget = AstUtils.getFunctionTarget(node);
+            if (functionTarget === 'document' || functionTarget === 'window.document') {
+                if (node.arguments.length === 1) {
+                    const functionName: string = AstUtils.getFunctionName(node);
+                    if (functionName === 'write') {
+                        ctx.addFailureAt(node.getStart(), node.getWidth(), Rule.WRITE_FAILURE);
+                    } else if (functionName === 'writeln') {
+                        ctx.addFailureAt(node.getStart(), node.getWidth(), Rule.WRITELN_FAILURE);
+                    }
                 }
             }
         }
 
-        super.visitCallExpression(node);
+        return ts.forEachChild(node, cb);
     }
+
+    return ts.forEachChild(ctx.sourceFile, cb);
 }

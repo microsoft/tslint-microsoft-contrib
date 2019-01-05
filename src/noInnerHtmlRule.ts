0312ea91add2fa106ee90e71cea7aa4fdff5e87f
andrew bernard <andrew.quartey@gmail.com>
2019-01-05 04:00:10 -0500

    convert no-inner-html rule to use a walk function (#765)

--- src/noInnerHtmlRule.ts
+++ src/noInnerHtmlRule.ts
@@ -1,8 +1,13 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
+import * as tsutils from 'tsutils';
 
 import { AstUtils } from './utils/AstUtils';
 import { ExtendedMetadata } from './utils/ExtendedMetadata';
+
+interface Options {
+    htmlLibExpressionRegex: RegExp;
+}
 
 const FAILURE_INNER: string = 'Writing a string to the innerHTML property is insecure: ';
 const FAILURE_OUTER: string = 'Writing a string to the outerHTML property is insecure: ';
@@ -25,48 +30,59 @@ export class Rule extends Lint.Rules.AbstractRule {
     };
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new NoInnerHtmlRuleWalker(sourceFile, this.getOptions()));
+        return this.applyWithFunction(sourceFile, walk, parseOptions(this.getOptions()));
     }
 }
 
-class NoInnerHtmlRuleWalker extends Lint.RuleWalker {
-    private readonly htmlLibExpressionRegex: RegExp = /^(jquery|[$])/i;
+function parseOptions(options: Lint.IOptions): Options {
+    let value: RegExp = /^(jquery|[$])/i;
 
-    constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
-        super(sourceFile, options);
-        const opt = this.getOptions();
-        if (typeof opt[1] === 'object' && opt[1]['html-lib-matcher']) {
-            this.htmlLibExpressionRegex = new RegExp(opt[1]['html-lib-matcher']);
-        }
+    const args: any[] = options.ruleArguments;
+    if (args && typeof args[1] === 'object' && args[1]['html-lib-matcher']) {
+        value = new RegExp(args[1]['html-lib-matcher']);
+    } else if (options instanceof Array && typeof options[1] === 'object' && options[1]['html-lib-matcher']) {
+        value = new RegExp(options[1]['html-lib-matcher']);
     }
 
-    protected visitBinaryExpression(node: ts.BinaryExpression): void {
-        // look for assignments to property expressions where the
-        // left hand side is either innerHTML or outerHTML
-        if (node.operatorToken.kind === ts.SyntaxKind.EqualsToken) {
-            if (node.left.kind === ts.SyntaxKind.PropertyAccessExpression) {
-                const propAccess: ts.PropertyAccessExpression = <ts.PropertyAccessExpression>node.left;
-                const propName: string = propAccess.name.text;
-                if (propName === 'innerHTML') {
-                    this.addFailureAt(node.getStart(), node.getWidth(), FAILURE_INNER + node.getText());
-                } else if (propName === 'outerHTML') {
-                    this.addFailureAt(node.getStart(), node.getWidth(), FAILURE_OUTER + node.getText());
+    return {
+        htmlLibExpressionRegex: value
+    };
+}
+
+function walk(ctx: Lint.WalkContext<Options>) {
+    const { htmlLibExpressionRegex } = ctx.options;
+
+    function cb(node: ts.Node): void {
+        if (tsutils.isBinaryExpression(node)) {
+            // look for assignments to property expressions where the
+            // left hand side is either innerHTML or outerHTML
+            if (node.operatorToken.kind === ts.SyntaxKind.EqualsToken) {
+                if (tsutils.isPropertyAccessExpression(node.left)) {
+                    const propAccess: ts.PropertyAccessExpression = node.left;
+                    const propName: string = propAccess.name.text;
+                    if (propName === 'innerHTML') {
+                        ctx.addFailureAt(node.getStart(), node.getWidth(), FAILURE_INNER + node.getText());
+                    } else if (propName === 'outerHTML') {
+                        ctx.addFailureAt(node.getStart(), node.getWidth(), FAILURE_OUTER + node.getText());
+                    }
                 }
             }
         }
-        super.visitBinaryExpression(node);
-    }
 
-    protected visitCallExpression(node: ts.CallExpression): void {
-        const functionName = AstUtils.getFunctionName(node);
-        if (functionName === 'html') {
-            if (node.arguments.length > 0) {
-                const functionTarget = AstUtils.getFunctionTarget(node);
-                if (functionTarget !== undefined && this.htmlLibExpressionRegex.test(functionTarget)) {
-                    this.addFailureAt(node.getStart(), node.getWidth(), FAILURE_HTML_LIB + node.getText());
+        if (tsutils.isCallExpression(node)) {
+            const functionName = AstUtils.getFunctionName(node);
+            if (functionName === 'html') {
+                if (node.arguments.length > 0) {
+                    const functionTarget = AstUtils.getFunctionTarget(node);
+                    if (functionTarget !== undefined && htmlLibExpressionRegex.test(functionTarget)) {
+                        ctx.addFailureAt(node.getStart(), node.getWidth(), FAILURE_HTML_LIB + node.getText());
+                    }
                 }
             }
         }
-        super.visitCallExpression(node);
+
+        return ts.forEachChild(node, cb);
     }
+
+    return ts.forEachChild(ctx.sourceFile, cb);
 }

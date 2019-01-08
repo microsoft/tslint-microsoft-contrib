3fccff9867d2ec0cfa877e6ee1772382640f9faf
Noam <noamyogev84@gmail.com>
2019-01-08 23:57:50 +0200

    refactor reactA11yIframsRule to use walk function add unit tests for class and function rule enforcement

--- src/reactA11yIframesRule.ts
+++ src/reactA11yIframesRule.ts
@@ -29,72 +29,59 @@ export class Rule extends Lint.Rules.AbstractRule {
     };
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return sourceFile.languageVariant === ts.LanguageVariant.JSX
-            ? this.applyWithWalker(new ReactA11yIframesRuleWalker(sourceFile, this.getOptions()))
-            : [];
+        return sourceFile.languageVariant === ts.LanguageVariant.JSX ? this.applyWithFunction(sourceFile, walk) : [];
     }
 }
 
-class ReactA11yIframesRuleWalker extends Lint.RuleWalker {
-    protected visitVariableDeclaration(node: ts.VariableDeclaration): void {
-        this.validate(node);
-        super.visitVariableDeclaration(node);
-    }
-
-    private validate(node: ts.Node) {
-        const titles: Set<string> = new Set();
-
-        this.getJsxElements(node).forEach((element: ts.Node) => {
-            const attributes = getJsxAttributesFromJsxElement(element);
-
-            // Validate that iframe has a non-empty title
-            const titleAttribute = attributes[TITLE_ATTRIBUTE_NAME];
-            const titleAttributeText = this.getAttributeText(titleAttribute);
-            if (!titleAttribute || !titleAttributeText) {
-                this.addFailureAt(element.getStart(), element.getWidth(), IFRAME_EMPTY_TITLE_ERROR_STRING);
-            }
-
-            // Validate the iframe title is unique
-            if (titleAttributeText && titles.has(titleAttributeText)) {
-                this.addFailureAt(element.getStart(), element.getWidth(), IFRAME_UNIQUE_TITLE_ERROR_STRING);
-            } else if (titleAttributeText) {
-                titles.add(titleAttributeText);
-            }
-
-            // Validate that iframe is not empty or hidden
-            const hiddenAttribute = attributes[HIDDEN_ATTRIBUTE_NAME];
-            const srcAttribute = attributes[SRC_ATTRIBUTE_NAME];
-            if (hiddenAttribute || !srcAttribute || !this.getAttributeText(srcAttribute)) {
-                this.addFailureAt(element.getStart(), element.getWidth(), IFRAME_EMPTY_OR_HIDDEN_ERROR_STRING);
-            }
-        });
-    }
-
-    private getJsxElements(node: ts.Node): (ts.JsxElement | ts.JsxSelfClosingElement)[] {
-        const elements: (ts.JsxElement | ts.JsxSelfClosingElement)[] = [];
-        tsutils.forEachToken(node, (childNode: ts.Node) => {
-            const parentNode = childNode.parent;
-            if (childNode.getText() === IFRAME_ELEMENT_NAME) {
-                if (tsutils.isJsxOpeningElement(parentNode)) {
-                    elements.push(parentNode.parent);
+function walk(ctx: Lint.WalkContext<void>) {
+    const previousTitles: Set<string> = new Set();
+    function cb(node: ts.Node): void {
+        if (
+            tsutils.isJsxFragment(node) ||
+            tsutils.isVariableDeclaration(node) ||
+            tsutils.isMethodDeclaration(node) ||
+            tsutils.isFunctionDeclaration(node)
+        ) {
+            previousTitles.clear();
+        }
+        if (tsutils.isJsxOpeningElement(node) || tsutils.isJsxSelfClosingElement(node)) {
+            if (node.tagName.getText() === IFRAME_ELEMENT_NAME) {
+                const attributes = getJsxAttributesFromJsxElement(node);
+                // Validate that iframe has a non-empty title
+                const titleAttribute = attributes[TITLE_ATTRIBUTE_NAME];
+                const titleAttributeText = getAttributeText(titleAttribute);
+                if (!titleAttribute || !titleAttributeText) {
+                    ctx.addFailureAtNode(node.tagName, IFRAME_EMPTY_TITLE_ERROR_STRING);
                 }
-                if (tsutils.isJsxSelfClosingElement(parentNode)) {
-                    elements.push(parentNode);
-                }
-            }
-        });
-        return elements;
-    }
 
-    private getAttributeText(attribute: ts.JsxAttribute): string | undefined {
-        if (attribute && attribute.initializer) {
-            if (tsutils.isJsxExpression(attribute.initializer)) {
-                return attribute.initializer.expression ? attribute.initializer.expression.getText() : undefined;
-            }
-            if (tsutils.isStringLiteral(attribute.initializer)) {
-                return attribute.initializer.text;
+                // Validate the iframe title is unique
+                if (titleAttributeText && previousTitles.has(titleAttributeText)) {
+                    ctx.addFailureAtNode(node.tagName, IFRAME_UNIQUE_TITLE_ERROR_STRING);
+                } else if (titleAttributeText) {
+                    previousTitles.add(titleAttributeText);
+                }
+
+                // Validate that iframe is not empty or hidden
+                const hiddenAttribute = attributes[HIDDEN_ATTRIBUTE_NAME];
+                const srcAttribute = attributes[SRC_ATTRIBUTE_NAME];
+                if (hiddenAttribute || !srcAttribute || !getAttributeText(srcAttribute)) {
+                    ctx.addFailureAtNode(node.tagName, IFRAME_EMPTY_OR_HIDDEN_ERROR_STRING);
+                }
             }
         }
-        return undefined;
+        return ts.forEachChild(node, cb);
     }
+    return ts.forEachChild(ctx.sourceFile, cb);
+}
+
+function getAttributeText(attribute: ts.JsxAttribute): string | undefined {
+    if (attribute && attribute.initializer) {
+        if (tsutils.isJsxExpression(attribute.initializer)) {
+            return attribute.initializer.expression ? attribute.initializer.expression.getText() : undefined;
+        }
+        if (tsutils.isStringLiteral(attribute.initializer)) {
+            return attribute.initializer.text;
+        }
+    }
+    return undefined;
 }

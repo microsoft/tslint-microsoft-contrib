310b54ef2c0f96e61280a90e5727224baeb0da78
IllusionMH <illusionmh@gmail.com>
2019-02-09 23:21:37 +0200

    create-rule task also updates README (fixed #816)

--- build-tasks/common/readme-rules.js
+++ build-tasks/common/readme-rules.js
@@ -17,6 +17,34 @@ function getAllRules() {
     return rules;
 }
 
+function addNewRule(name, ruleMarkup) {
+    let match;
+    let insertPosition;
+
+    while ((match = praseRe.exec(readmeContent))) {
+        const existingName = match[1];
+        // Looks for first rule that should follow new rule.
+        // Match position will be used to insert new rule row
+        if (name < existingName) {
+            insertPosition = match.index;
+            break;
+        }
+    }
+
+    // In case when new rule should be added to the end of the table - look for insret position before </tbody>
+    if (insertPosition === undefined) {
+        insertPosition = readmeContent.match(/\s+<\/tbody>\s+<\/table>/).index;
+    }
+    const contentBeforeNewRule = readmeContent.slice(0, insertPosition);
+    const contentAfterNewRule = readmeContent.slice(insertPosition);
+    const newContent = contentBeforeNewRule + ruleMarkup + contentAfterNewRule;
+    writeFile(README_NAME, newContent);
+
+    // To position cursor on the same line with new rule name should add 3 lines to match lines added with template
+    return `./${README_NAME}:${contentBeforeNewRule.split('\n').length + 3}`;
+}
+
 module.exports = {
-    getAllRules
+    getAllRules,
+    addNewRule
 };

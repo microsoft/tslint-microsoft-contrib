bfc28e95dde1fab92aa457e79d338fb03b81d7cc
Andrii Dieiev <illusionmh@gmail.com>
2019-07-29 12:34:57 +0300

    build: enable npm package publication from master branch (#878)
    * build: make dist/build ready for direct publishing
    
    * build: prevent unintentional npm publish from root
    
    * docs: update release steps for npm package publish

--- build-tasks/generate-package-json-for-npm.js
+++ build-tasks/generate-package-json-for-npm.js
@@ -6,5 +6,6 @@ const { readJSON, writeFile } = require('./common/files');
 
 const basePackageJson = readJSON('package.json');
 delete basePackageJson.devDependencies;
+delete basePackageJson.scripts;
 
 writeFile('dist/build/package.json', JSON.stringify(basePackageJson, undefined, 4));
